faeeed5817653b10b0a836d456d299e971fd097a
Rezapu Obed <obed.ubaidillah@gmail.com>
2022-11-12 22:20:30 +0700

    fix: no more tuple

--- avatar.py
+++ avatar.py
@@ -5,7 +5,7 @@ from typing import overload
 class Avatar:
     def __init__(self, user_id: int) -> None:
         self._user_id = user_id
-        self.details:dict[discord.Guild.id, tuple[str, str]] = dict() # {guild_id : (name, asset_url)}
+        self.details:dict[discord.Guild.id, dict[str, str]] = dict() # {guild_id : {name, asset_url}}
 
     
     def get_user_id(self) -> int:
@@ -13,7 +13,7 @@ class Avatar:
 
 
     def get_user(self, bot: discord.Bot) -> discord.User:
-        return bot.get_user(self._id)
+        return bot.get_user(self._user_id)
 
 
     def get_member(self, guild: discord.Guild) -> discord.Member:
@@ -22,7 +22,7 @@ class Avatar:
 
     def get_details_in_guild(self, guild: discord.Guild) -> tuple[str, str]:
         guild_id = guild.id
-        return (self.details.get(guild_id).name, self.details.get(guild_id).asset_url)
+        return (self.details.get(guild_id).get('name'), self.details.get(guild_id).get('asset_url'))
 
 
     def is_defined_in_guild(self, guild: discord.Guild) -> bool:
@@ -31,7 +31,7 @@ class Avatar:
 
     @overload
     def set_detail_in_guild(self, name: str, asset_url: str, guild: discord.Guild) -> None:
-        detail = {guild.id: (name, asset_url)}
+        detail = {guild.id: {'name':name, 'asset_url':asset_url}}
         self.details.update(detail)
 
 
@@ -40,20 +40,19 @@ class Avatar:
 
 
     def dumps(self) -> dict:
-        json_details = json.loads(json.dumps(self.details))
+        json_avatar = json.loads(json.dumps(self.__dict__))
+        json_avatar.update({'_id': str(self._user_id)})
+        json_avatar.pop('_user_id')
 
-        return {'_id': str(self._user_id), 'details': json_details}
+        return json_avatar
 
 
     @staticmethod
     def loads(json_avatar: dict):
         new = Avatar(0)
         new._user_id = int(json_avatar.get('_id'))
-        temp = tuple()
 
-        for key in json_avatar.get('details'):
-            for item in json_avatar.get('details').values():
-                temp += tuple(item)
-            new.details.update({int(key): temp})
+        new.details = {int(key):json_avatar.get('details').get(key) for key in json_avatar.get('details')}
+
         return new
 
