7403b997a3b041910382ac354bd95e93dadaf6e7
Rezapu Obed <obed.ubaidillah@gmail.com>
2022-11-12 21:13:38 +0700

    feat: base mongodb

---
+++ database.py
@@ -0,0 +1,43 @@
+import pymongo
+from avatar import Avatar
+
+def get_db(name:str ='discord') -> pymongo.database.Database:
+    CONNECTION_STRING = 'mongodb+srv://obediqbal:obedganteng2@cluster0.rxqwuvv.mongodb.net/test'
+    client = pymongo.MongoClient(CONNECTION_STRING)
+
+    return client[name]
+
+
+def update_avatar_to_db(avatar: Avatar) -> None:
+    dcdb = get_db()
+    avatars_collection = dcdb['avatars']
+    user_id = str(avatar.get_user_id())
+
+    cursor = avatars_collection.find({'_id': user_id})[0]
+    print(cursor)
+
+db = get_db()
+avatars_col = db['avatars']
+new_avatar = Avatar(123)
+detail = {0: ('obed', 'https://google.com')}
+new_avatar.set_detail_in_guild(detail)
+
+print(new_avatar.dumps())
+
+# avatars_col.insert_one()
+
+
+
+
+# myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+
+# mydb = myclient['database']
+
+# mycol = mydb['customers']
+
+# mydict = {'name':'obed','age':19}
+
+# x = mycol.insert_one(mydict)
+
+# print(x.inserted_id)
+# print(type(mydb))
