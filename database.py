c53634373b10f0b8c300e20556c1f1e403c4a4f0
Rezapu Obed <obed.ubaidillah@gmail.com>
2022-11-13 00:28:25 +0700

    fix: base update avatar to db

--- database.py
+++ database.py
@@ -15,34 +15,23 @@ def get_db(name:str ='discord') -> pymongo.database.Database:
 
 def update_avatar_to_db(avatar: Avatar) -> None:
     dcdb = get_db()
-    avatars_collection = dcdb['avatars']
-    user_id = str(avatar.get_user_id())
+    avatars_col = dcdb['avatars']
 
-    cursor = avatars_collection.find({'_id': user_id})[0]
-    print(cursor)
+    avatar_json = avatar.dumps()
+    query = {"_id": avatar_json.get('_id')}
+    avatars_col.update_one(query, {'$set': avatar_json})
+    
 
-db = get_db()
-avatars_col = db['avatars']
-new_avatar = Avatar(123)
-detail = {0: ('obed', 'https://google.com')}
-new_avatar.set_detail_in_guild(detail)
-
-print(new_avatar.dumps())
-
-# avatars_col.insert_one()
-
-
-
-
-# myclient = pymongo.MongoClient('mongodb://localhost:27017/')
-
-# mydb = myclient['database']
-
-# mycol = mydb['customers']
-
-# mydict = {'name':'obed','age':19}
-
-# x = mycol.insert_one(mydict)
-
-# print(x.inserted_id)
-# print(type(mydb))
+# db = get_db()
+# avatars_col = db['avatars']
+# new_avatar = Avatar(555)
+# # detail = {8: ('rezapu', 'https://yahoo.com')}
+# detail = {31: {
+#     'name': 'obed',
+#     'asset_url': 'https://yahoo.com'
+# }}
+# new_avatar.set_detail_in_guild(detail)
+# detail = {8: {
+#     'name': 'rezapu',
+#     'asset_url': 'https://google.com'
+# }}
