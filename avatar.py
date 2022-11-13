adf7581ce2bf9bebbd1a2f920182ab053be4fcba
Rezapu Obed <obed.ubaidillah@gmail.com>
2022-11-13 15:28:49 +0700

    feat: setter for user id

--- avatar.py
+++ avatar.py
@@ -4,6 +4,7 @@ from typing import overload
 
 class Avatar:
     def __init__(self, user_id: int) -> None:
+        """Create an instance of Avatar with no details"""
         self._user_id = user_id
         self.details:dict[discord.Guild.id, dict[str, str]] = dict() # {guild_id : {name, asset_url}}
 
@@ -12,12 +13,16 @@ class Avatar:
         return self._user_id
 
 
+    def set_user_id(self, user_id: int) -> None:
+        self._user_id = user_id
+
+
     def get_user(self, bot: discord.Bot) -> discord.User:
-        return bot.get_user(self._user_id)
+        return bot.get_user(self.get_user_id())
 
 
     def get_member(self, guild: discord.Guild) -> discord.Member:
-        return guild.get_member(self._user_id)
+        return guild.get_member(self.get_user_id())
 
 
     def get_detail_in_guild(self, guild: discord.Guild) -> tuple:
@@ -43,7 +48,7 @@ class Avatar:
 
     def dumps(self) -> dict:
         json_avatar = json.loads(json.dumps(self.__dict__))
-        json_avatar.update({'_id': str(self._user_id)})
+        json_avatar.update({'_id': str(self.get_user_id())})
         json_avatar.pop('_user_id')
 
         return json_avatar
@@ -52,9 +57,19 @@ class Avatar:
     @staticmethod
     def loads(json_avatar: dict):
         new = Avatar(0)
-        new._user_id = int(json_avatar.get('_id'))
+        new.set_user_id(int(json_avatar.get('_id')))
 
         new.details = {int(key):json_avatar.get('details').get(key) for key in json_avatar.get('details')}
 
         return new
+
+
+    # @staticmethod
+    # def parse(user: discord.user.User):
+    #     new = Avatar(0)
+    #     new.set_user_id(user.id)
+
+    #     new.details = {}
+
+    #     return new
 
