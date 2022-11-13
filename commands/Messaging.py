91c983344f69efa95c1b7744744efa0db0b94ccd
Rezapu Obed <obed.ubaidillah@gmail.com>
2022-11-13 15:55:08 +0700

    feat: error handling on private message

--- commands/Messaging.py
+++ commands/Messaging.py
@@ -4,21 +4,25 @@ from discord.ext import commands
 from avatar import Avatar
 import database as db
 
+#https://i.imgur.com/dOzAFCx.png
 class Messaging(commands.Cog):
     def __init__(self, bot:commands.Bot):
         self.bot = bot
 
-
     @commands.guild_only()
     @discord.slash_command(name='send', description='Send a message anonymously through your avatar')
+    @discord.commands.option(
+        "message",
+        description = 'Enter your message',
+        required = True
+    )
     async def _send_message(self, interaction: discord.Interaction, message: str):
         avatar = db.get_avatar_from_db(interaction.user.id)
-
         if(avatar == None): # User has never used the bot
             avatar = Avatar(interaction.user.id)
             db.insert_avatar_to_db(avatar)
         if(not avatar.is_defined_in_guild(interaction.guild)): # User has never used the bot in this guild
-            avatar.set_detail_in_guild('Anonymous', 'https://i.imgur.com/dOzAFCx.png', interaction.guild)
+            avatar.set_detail_in_guild('Anonymous', '', interaction.guild)
         db.update_avatar_to_db(avatar)
 
         (name, avatar_url) = avatar.get_detail_in_guild(interaction.guild)
@@ -32,6 +36,12 @@ class Messaging(commands.Cog):
     @commands.command()
     async def ping(self, ctx):
         await ctx.send('Pong!')
+
+
+    @_send_message.error
+    async def _send_message_error(self, interaction:discord.Interaction, error):
+        if isinstance(error, commands.errors.NoPrivateMessage):
+            await interaction.response.send_message(error)
 
 
 def setup(bot: discord.Bot):
