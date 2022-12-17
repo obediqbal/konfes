ddad3b504939cddf4d4ae803f2a20523612db5e3
Rezapu Obed <obed.ubaidillah@gmail.com>
2022-12-17 17:30:02 +0700

    feat: get refer embed

--- src/utils.py
+++ src/utils.py
@@ -21,3 +21,36 @@ class Utils:
             files.append(await Utils.load_image_from_url(url, parse_to_discord=parse_to_discord))
 
         return files
+
+    
+    def get_refer_embed(refer: discord.Message):
+        if refer==None:
+            return None
+
+        if len(refer.content) > 300:
+            refer.content = refer.content[:200]
+            refer.content += f' [...]({refer.jump_url})'
+
+        if refer.webhook_id != None:
+            color = 14079702
+        else:
+            color = refer.author.color.value
+
+        attachment = ''
+        if len(refer.attachments)>0:
+            attachment = ' <:konfesimage:1052941241811210300>'
+
+        embed_dict = {
+                'type': 'rich',
+                'color': color,
+                'description': f'[\[#\]]({refer.jump_url}) {refer.content}' + attachment,
+                'author': {
+                    'name': refer.author.display_name,
+                    'url': refer.jump_url,
+                    'icon_url': refer.author.display_avatar.url
+                },
+                # "timestamp": message.created_at.isoformat('T')
+            }
+        embed = discord.Embed.from_dict(embed_dict)
+        
+        return embed
